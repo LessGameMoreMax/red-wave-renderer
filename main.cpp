fc5298a2a3a469d84478b44d2dbe37df84c6df37
LessGameMoreMax <LessGameMoreMax@outlook.com>
2023-05-06 13:55:16 +0800

    23/05/06

--- main.cpp
+++ main.cpp
@@ -7,28 +7,28 @@ using namespace sablin;
 int main(){
     ModelPool::Create();
     Model *cube = Loader::LoadOBJModel(
-                "/home/sablin/Projects/soft-rtr/Assets/Models/dragon/dragon.obj");
+                "/home/sablin/Projects/soft-rtr/Assets/Models/cube/cube.obj");
     // Model *cube = Loader::LoadOBJModel(
     //             "/home/sablin/Projects/soft-rtr/Assets/Models/teapot/teapot.obj");
 
-    // for(int64_t i = 0;i != cube->mesh_->triangle_pool_size_; ++i){
-    //     std::cout << "Triangle: " << i << std::endl;
-    //     Triangle *t = cube->mesh_->triangle_pool_ + i;
-    //     std::cout << t->vertex_a_.normal_->x_ << std::endl;
-    //     std::cout << t->vertex_a_.normal_->y_ << std::endl;
-    //     std::cout << t->vertex_a_.normal_->z_ << std::endl;
-    //     std::cout << t->vertex_a_.normal_->w_ << std::endl;
-    //     std::cout << std::endl;
-    //     std::cout << t->vertex_b_.normal_->x_ << std::endl;
-    //     std::cout << t->vertex_b_.normal_->y_ << std::endl;
-    //     std::cout << t->vertex_b_.normal_->z_ << std::endl;
-    //     std::cout << t->vertex_b_.normal_->w_ << std::endl;
-    //     std::cout << std::endl;
-    //     std::cout << t->vertex_c_.normal_->x_ << std::endl;
-    //     std::cout << t->vertex_c_.normal_->y_ << std::endl;
-    //     std::cout << t->vertex_c_.normal_->z_ << std::endl;
-    //     std::cout << t->vertex_c_.normal_->w_ << std::endl;
-    // }
+    for(int64_t i = 0;i != cube->mesh_->triangle_pool_size_; ++i){
+        std::cout << "Triangle: " << i << std::endl;
+        Triangle *t = cube->mesh_->triangle_pool_ + i;
+        std::cout << t->vertex_a_.normal_->x_ << std::endl;
+        std::cout << t->vertex_a_.normal_->y_ << std::endl;
+        std::cout << t->vertex_a_.normal_->z_ << std::endl;
+        std::cout << t->vertex_a_.normal_->w_ << std::endl;
+        std::cout << std::endl;
+        std::cout << t->vertex_b_.normal_->x_ << std::endl;
+        std::cout << t->vertex_b_.normal_->y_ << std::endl;
+        std::cout << t->vertex_b_.normal_->z_ << std::endl;
+        std::cout << t->vertex_b_.normal_->w_ << std::endl;
+        std::cout << std::endl;
+        std::cout << t->vertex_c_.normal_->x_ << std::endl;
+        std::cout << t->vertex_c_.normal_->y_ << std::endl;
+        std::cout << t->vertex_c_.normal_->z_ << std::endl;
+        std::cout << t->vertex_c_.normal_->w_ << std::endl;
+    }
 
     ModelPool::GetSingleton()->Destroy();
     return 0;
