bb47ddcdc1895b91ac992842e33a12f335ba59e0
LessGameMoreMax <LessGameMoreMax@outlook.com>
2023-06-10 12:49:23 +0800

    23/06/10

--- src/renderer/OutputMerger.cpp
+++ src/renderer/OutputMerger.cpp
@@ -7,21 +7,20 @@ void OutputMerger::DepthTest(Fragment *fragment){
     int32_t index = fragment->screen_coord_.y_ * 
         fragment->scene_->GetFrameWidth() + fragment->screen_coord_.x_;
 
-    float alpha = fragment->material_->d_;
-
-    if(std::abs(alpha - 1.0f) > FLOAT_ERROR){
-        fragment->scene_->depth_buffer_lock_.lock();
-        fragment->scene_->GetFrame()->get_colors_()[index] =
-                fragment->color_ * alpha +
-                fragment->scene_->GetFrame()->get_colors_()[index] * (1.0f -  alpha);
-        fragment->scene_->depth_buffer_lock_.unlock();
-    }else{
-        fragment->scene_->depth_buffer_lock_.lock();
-        if(fragment->depth_ > fragment->scene_->GetDepthBuffer()[index]){
+    if(fragment->depth_ > fragment->scene_->GetDepthBuffer()[index]){
+        float alpha = fragment->material_->d_;
+        if(std::abs(alpha - 1.0f) > FLOAT_ERROR){
+            fragment->scene_->depth_buffer_lock_.lock();
+            fragment->scene_->GetFrame()->get_colors_()[index] =
+                    fragment->color_ * alpha +
+                    fragment->scene_->GetFrame()->get_colors_()[index] * (1.0f -  alpha);
+            fragment->scene_->depth_buffer_lock_.unlock();
+        }else{
+            fragment->scene_->depth_buffer_lock_.lock();
             fragment->scene_->GetFrame()->get_colors_()[index] = fragment->color_;
             fragment->scene_->GetDepthBuffer()[index] = fragment->depth_;
+            fragment->scene_->depth_buffer_lock_.unlock();
         }
-        fragment->scene_->depth_buffer_lock_.unlock();
     }
     //TODO: No lock to speed
     return;
